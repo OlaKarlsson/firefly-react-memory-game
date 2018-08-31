38abfea2c1277ec5d785e7f7420f20f6b606071b
Ola Karlsson <ola@olakarlsson.com>
2018-08-31 13:38:34 +0200

    Created files

---
+++ src/welcomeScreen.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './welcomescreen.css';
+
+
+export default class WelcomeScreen extends Component{
+
+    render(){
+        return(<h1>Welcome!</h1>)
+    }
+        
+}
