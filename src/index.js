98e984cfd1b345003ac192ae7e552e3ba1084bf5
Ola Karlsson <ola@olakarlsson.com>
2018-11-14 16:01:06 +0100

    Added functionality for Installing web app to home screen

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 
+// Register service worker to control making site work offline
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('/service-worker.js')
+             .then(function() { console.log('Service Worker Registered'); });
+  }
+
+
+  
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
