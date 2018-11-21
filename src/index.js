176d20a0d5fd97d458df36833b33c1e5f1375930
Ola Karlsson <ola@olakarlsson.com>
2018-11-21 14:14:21 +0100

    Added comments

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './app';
 
 // Register service worker to control making site work offline
+// This is one of the requirements for making the app "installable" through Chrome
+// https://developers.google.com/web/fundamentals/app-install-banners/
 if('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('service-worker.js')
